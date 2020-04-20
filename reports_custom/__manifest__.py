8d2b93e1500b9dac21fae7cd7143834a46cc85b8
Raül Carbonell <raul.carbonell@processcontrol.es>
2020-04-21 00:20:49 +0200

    [ADD] Report custom

---
+++ reports_custom/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Raul Carbonell - raul.carbonell@processcontrol.es
+# License AGPL-3.0 or later
+{
+    "name": "Informes",
+    "summary":
+        "Informe Curso",
+    "version": "13.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "website": "",
+    "author": "Raul Carbonell",
+    "license": "AGPL-3",
+    "data": [
+        "reports/sale_order_report_custom.xml",        
+    ],
+    "depends": [
+        "base",
+        "sale",
+    ],
+    "application": True,
+    "installable": True,
+}
